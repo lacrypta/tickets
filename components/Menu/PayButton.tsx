7f3d2a2707354ae563fd13ab062dddbd9250f2c5
Agustin Kassis <webmaster@masize.com>
2022-09-14 10:36:44 -0300

    disable pay button on empty cart

--- components/Menu/PayButton.tsx
+++ components/Menu/PayButton.tsx
@@ -1,12 +1,15 @@
 import { Fab } from "@mui/material";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useContext } from "react";
+import { CartContext } from "../../providers/Cart";
 
 interface IPayButtonProps {
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 const PayButton = ({ onClick }: IPayButtonProps) => {
+  const { cart } = useContext(CartContext);
+
   return (
     <Fab
       variant='extended'
@@ -14,6 +17,7 @@ const PayButton = ({ onClick }: IPayButtonProps) => {
       color='primary'
       sx={{ position: "fixed", bottom: 16, right: 16 }}
       aria-label='add'
+      disabled={cart.total <= 0}
       onClick={onClick}
     >
       <ShoppingCartCheckoutIcon sx={{ mr: 1 }} />

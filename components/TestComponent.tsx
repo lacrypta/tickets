bebb6db9e111d320c0f1b0e03cc6f5a848e2b01b
Agustin Kassis <webmaster@masize.com>
2022-09-23 10:20:27 -0300

    updated tester

--- components/TestComponent.tsx
+++ components/TestComponent.tsx
@@ -1,32 +1,60 @@
 import { useEffect, useState } from "react";
-import { db, collection, onSnapshot } from "../lib/public/firebase";
+import { db, collection, onSnapshot, doc } from "../lib/public/firebase";
+
+const User = ({ user }: any) => {
+  if (!user) {
+    return "";
+  }
+  return (
+    <div key={user.id}>
+      <div>Username: {user.username}</div>
+      <div>Address: {user.permit.owner}</div>
+      <div>
+        <textarea>{JSON.stringify(user.permit)}</textarea>
+      </div>
+    </div>
+  );
+};
 
 const TestComponent = () => {
   const [usersList, setUsersList] = useState<any>([]);
+  const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
-    const usersCol = collection(db, "users");
-    onSnapshot(usersCol, {
+    // Multiple
+    const usersRef = collection(db, "users");
+    onSnapshot(usersRef, {
       next: (snapshot) => {
         setUsersList(snapshot.docs.map((doc) => doc.data()));
         console.dir(snapshot.docs[0].data());
+      },
+    });
+
+    // Single
+    const userRef = doc(
+      db,
+      "users",
+      "0xFA14B3b6104A64F676A170C61A93e17556CE128e"
+    );
+    onSnapshot(userRef, {
+      next: (snapshot) => {
+        console.info("User updated");
+        console.dir(snapshot.data());
+        setUser(snapshot.data());
       },
     });
   }, []);
 
   return (
     <div>
-      {usersList.map((user: any) => {
-        return (
-          <div key={user.id}>
-            <div>Username: {user.username}</div>
-            <div>Address: {user.permit.owner}</div>
-            <div>
-              <textarea>{JSON.stringify(user.permit)}</textarea>
-            </div>
-          </div>
-        );
+      <h2>List</h2>
+      {usersList.map((user: any, k: string) => {
+        return <User key={k} user={user} />;
       })}
+      <div>
+        <h2>Single</h2>
+        <User user={user} />
+      </div>
     </div>
   );
 };

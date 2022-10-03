415a942f71f5bc6679e31a6d00392bf5b8200411
Agustin Kassis <webmaster@masize.com>
2022-10-03 10:22:45 -0300

    updated pay api request

--- pages/api/gateway/pay.ts
+++ pages/api/gateway/pay.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { addPayment } from "../../../lib/private/firestore";
+import { IPaymentRequestBody, PaymentSchema } from "../../../types/request";
 
 type Data = {
   success: boolean;
@@ -6,14 +8,23 @@ type Data = {
   data?: any;
 };
 
-const request = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const request = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.method !== "POST") {
     res
       .status(405)
       .json({ success: false, message: "Only POST method is allowed" });
   }
-  const permit = req.body;
-  res.status(200).json({ success: true, data: permit });
+
+  try {
+    PaymentSchema.parse(req.body);
+  } catch (e) {
+    res.status(400).json({ success: false, message: "Malformed request" });
+  }
+
+  const payment: IPaymentRequestBody = req.body;
+  const paymentId = await addPayment(payment.order, payment.voucher);
+
+  res.status(200).json({ success: true, data: { paymentId } });
 };
 
 export default request;

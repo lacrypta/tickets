042feaca46488819d03394568f0de932255bee88
Agustin Kassis <webmaster@masize.com>
2022-10-05 09:56:43 -0300

    get total

--- lib/public/menu.ts
+++ lib/public/menu.ts
@@ -1,9 +1,32 @@
 // Menu functions
 
 import menuItems from "../../data/menu.json";
+import { IOrderItem } from "../../types/cart";
+
+const indexedMenuPrices: { [index: string]: number } = {};
 
 const getMenuItems = () => {
   return menuItems;
 };
 
-export { getMenuItems };
+const generateIndex = () => {
+  menuItems.forEach((item) => {
+    indexedMenuPrices[item.id] = item.price;
+  });
+  indexedMenuPrices;
+};
+
+const getTotal = (items: IOrderItem[]) => {
+  let total = 0;
+
+  Object.values(items).map((item) => {
+    // eslint-disable-next-line no-undef
+    total += item.qty * indexedMenuPrices[item.id];
+  });
+  return total;
+};
+
+// Just once
+generateIndex();
+
+export { getMenuItems, getTotal };

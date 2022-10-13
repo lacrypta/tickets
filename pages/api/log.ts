be9017ee2ac431242f82e147076ab0db649437fe
Agustin Kassis <webmaster@masize.com>
2022-10-13 12:39:45 -0300

    add empty object on log

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { log } from "../../lib/private/firestore";
 
 const request = async (req: NextApiRequest, res: NextApiResponse) => {
-  log("log", req.body);
+  log("log", req.body || {});
   res.status(200).json({ success: true, data: "OK" });
 };
 

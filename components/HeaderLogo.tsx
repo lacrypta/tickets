27603844cc99c355fab118aee2fb11f3b94e5967
Agustin Kassis <webmaster@masize.com>
2022-10-14 16:29:24 -0300

    added halloween

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -1,16 +1,35 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
 import brandLogo from "../public/images/lacrypta-title.svg";
+import halloweenLogo from "../public/images/halloween.png";
 
 const Container = styled.div`
   width: 100%;
   margin-bottom: 2em;
 `;
 
+const Logo = styled(Image)``;
+
+const HalloweenDiv = styled.div`
+  width: 45%;
+  position: relative;
+  right: -55%;
+  top: -12px;
+`;
+
+const Halloween = styled(Image)``;
+
 export const HeaderLogo = () => {
   return (
     <Container>
-      <Image alt='La Crypta bar' src={brandLogo} />
+      <Logo alt='La Crypta bar' src={brandLogo} />
+      <HalloweenDiv>
+        <Halloween
+          layout='responsive'
+          alt='La Crypta Halloween'
+          src={halloweenLogo}
+        />
+      </HalloweenDiv>
     </Container>
   );
 };

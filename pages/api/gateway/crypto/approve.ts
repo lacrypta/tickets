3486d48ff136e589ff4038d8a24613b8411f0486
Agustin Kassis <webmaster@masize.com>
2022-10-15 16:12:02 -0300

    event webhook

--- pages/api/gateway/crypto/approve.ts
+++ pages/api/gateway/crypto/approve.ts
@@ -1,15 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { sendEmail } from "../../../../lib/private/email";
-import { getOrder, updateOrder } from "../../../../lib/private/firestore";
+import { parseUnits } from "ethers/lib/utils";
+
+import {
+  addTx,
+  getOrder,
+  isTxStored,
+  updateOrder,
+} from "../../../../lib/private/firestore";
+import { getTransferEvent } from "../../../../lib/private/blockchain";
 import {
   CreateCryptoPaymentRequestSchema,
   ICreateCryptoPaymentRequestBody,
 } from "../../../../types/request";
+import { sendEmail } from "../../../../lib/private/email";
+
+const PERONIO_ADDRESS = process.env.NEXT_PUBLIC_PERONIO_ADDRESS || "";
+const BAR_ADDRESS = process.env.NEXT_PUBLIC_BAR_ADDRESS || "";
+const TICKET_PRICE = process.env.NEXT_PUBLIC_TICKET_PRICE_PE || "";
 
 const request = async (req: NextApiRequest, res: NextApiResponse) => {
   // Requires POST
-
-  console.info("Pre post");
   if (req.method !== "POST") {
     res
       .status(405)
@@ -17,8 +27,6 @@ const request = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  console.info("Validate");
-  console.dir(req.body);
   // Validate body type
   try {
     CreateCryptoPaymentRequestSchema.parse(req.body);
@@ -32,11 +40,53 @@ const request = async (req: NextApiRequest, res: NextApiResponse) => {
   const { address, amount, orderId, tx }: ICreateCryptoPaymentRequestBody =
     req.body;
 
+  let txEvent;
+  try {
+    // Check if tx already claimed
+    if (await isTxStored(tx)) {
+      throw new Error("Transaction already claimed");
+    }
+
+    txEvent = await getTransferEvent(tx);
+
+    // Event Validation
+    if (txEvent.raw.transactionHash !== tx) {
+      throw new Error("Invalid transaction hash");
+    }
+    if (txEvent.raw.address !== PERONIO_ADDRESS) {
+      throw new Error("Invalid contract address");
+    }
+    if (txEvent.decoded.from !== address) {
+      throw new Error("Invalid From");
+    }
+    if (txEvent.decoded.to !== BAR_ADDRESS) {
+      throw new Error("Invalid to");
+    }
+    if (!txEvent.decoded.value.eq(parseUnits(TICKET_PRICE, 6))) {
+      throw new Error("Invalid amount");
+    }
+  } catch (e: any) {
+    res.status(500).json({
+      success: false,
+      message: e.message,
+      peAddress: PERONIO_ADDRESS,
+      currentAddress: txEvent?.raw.address,
+    });
+    return;
+  }
+
+  // Add transaction to the storage
+  addTx({
+    contractAddress: PERONIO_ADDRESS,
+    from: address,
+    hash: tx,
+    orderId,
+    to: BAR_ADDRESS,
+    value: parseFloat(amount),
+  });
+
   // Get order
   const order = await getOrder(orderId);
-
-  console.info("Get Order:");
-  console.dir(order);
 
   // Validate existance
   if (!order) {
@@ -45,25 +95,20 @@ const request = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   if (order.status === "pending") {
-    // **************** SEND Email **************** //
+    // Sends Email
     await sendEmail({
       fullname: order.fullname,
       email: order.email,
       url: "https://entradas.lacrypta.com.ar/entrada/" + orderId,
     });
-
-    console.info("Email sent");
-
     // Updates order
-    const debug = await updateOrder(orderId, {
+    await updateOrder(orderId, {
       address,
       amount,
       tx,
       payment_method: "crypto",
       status: "completed",
     });
-
-    console.dir(debug);
   }
 
   res.status(200).json({

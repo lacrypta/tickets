2ddd9946475cfc646266a359ea53f610b89059b3
Agustin Kassis <webmaster@masize.com>
2022-10-17 10:52:18 -0300

    refactored index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,15 @@
 import styled from "@emotion/styled";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useAccount, useNetwork } from "wagmi";
+import { useAccount } from "wagmi";
 
 import { HeaderLogo } from "../components/HeaderLogo";
 import { Footer } from "../components/Footer";
 import { Background } from "../components/Background";
 import Header from "../components/Header";
 
-import DisconnectedWidget from "../components/Widgets/DisconnectedWidget";
 import MainWidget from "../components/Widgets/MainWidget";
-import InvalidNetworkWidget from "../components/Widgets/InvalidNetworkWidget";
 import useUser from "../hooks/useUser";
-import SignupWidget from "../components/Widgets/SignupWidget";
 import { useEffect, useState } from "react";
 import useLoading from "../hooks/useLoading";
 
@@ -27,26 +24,25 @@ const MainBlock = styled.main`
 
 const Home: NextPage = () => {
   const { isDisconnected } = useAccount();
-  const { chain } = useNetwork();
-  const [isMounted, setIsMounted] = useState(false); // Fix Hydration trouble
-  const { isRegistered, isLoading } = useUser();
+  // const { chain } = useNetwork();
+  const { isLoading } = useUser();
   const { setActive } = useLoading();
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     setActive(isLoading);
   }, [isLoading, setActive]);
 
-  const ValidNetworkBlock = () => {
-    return isRegistered ? <MainWidget /> : <SignupWidget />;
-  };
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  // const ValidNetworkBlock = () => {
+  //   return isRegistered ? <MainWidget /> : <SignupWidget />;
+  // };
 
-  const ConnectedBlock = () => {
-    return chain?.id === 137 ? <ValidNetworkBlock /> : <InvalidNetworkWidget />;
-  };
+  // const ConnectedBlock = () => {
+  //   return chain?.id === 137 ? <ValidNetworkBlock /> : <InvalidNetworkWidget />;
+  // };
 
   return (
     <div>
@@ -56,20 +52,12 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      {!isDisconnected ? <Header /> : ""}
+      <Header />
 
       <MainBlock>
         <Background />
         <HeaderLogo />
-        {isMounted ? (
-          isDisconnected ? (
-            <DisconnectedWidget />
-          ) : (
-            <ConnectedBlock />
-          )
-        ) : (
-          "Cargando..."
-        )}
+        {isMounted ? <MainWidget /> : "Cargando..."}
       </MainBlock>
 
       <Footer />

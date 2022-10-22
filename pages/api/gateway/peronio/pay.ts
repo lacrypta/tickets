9661f30bbd057254e971be4a5f23c7aec70b7c93
Agustin Kassis <webmaster@masize.com>
2022-10-22 17:38:08 -0300

    completed final payment flow

--- pages/api/gateway/peronio/pay.ts
+++ pages/api/gateway/peronio/pay.ts
@@ -12,8 +12,6 @@ import {
 import { addERC20Payment } from "../../../../lib/private/firestore";
 import { serveVoucher } from "../../../../lib/private/blockchain";
 
-const BAR_ADDRESS = process.env.NEXT_PUBLIC_BAR_ADDRESS;
-
 type Data = {
   success: boolean;
   message?: string;
@@ -38,11 +36,16 @@ const request = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   }
 
   const { voucher, signature } = requestData.voucher;
-  const payload = decodePayload(voucher.payload);
 
+  let txHash;
   try {
-    if (payload.to !== BAR_ADDRESS) {
-      throw new Error("Invalid destination");
+    const payload = decodePayload(voucher.payload);
+    // Broadcast
+    try {
+      txHash = await serveVoucher(encodeVoucher(voucher), signature);
+    } catch (e: any) {
+      console.error(e);
+      throw new Error("Transaction not broaadcasted");
     }
 
     // Add
@@ -52,17 +55,14 @@ const request = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       payload
     );
 
-    // Broadcast
-    console.info("Needs to broadcast");
-    console.info("Payment ID: " + paymentId);
-    try {
-      const tx = await serveVoucher(encodeVoucher(voucher), signature);
-      console.info("Broadcaster TX!:");
-      console.dir(tx);
-    } catch (e: any) {
-      console.error(e);
-      throw new Error("Transaction not broaadcasted");
-    }
+    // Reply OK
+    res.status(200).json({
+      success: true,
+      data: {
+        paymentId,
+        txHash,
+      },
+    });
   } catch (e: any) {
     console.error(e);
     res.status(406).json({ success: false, message: e.message });

719ff7dcb3db7c0ddf4d2c353e0bacef2a6e8995
Agustin Kassis <webmaster@masize.com>
2022-10-22 12:19:35 -0300

    added run permit

--- lib/private/blockchain.ts
+++ lib/private/blockchain.ts
@@ -1,17 +1,21 @@
 import { ITransferVoucher } from "./../../plugins/gateway/types/Voucher";
-import { ethers } from "ethers";
+import { Contract, ethers } from "ethers";
 import { Interface } from "ethers/lib/utils";
 
 import {
   address,
-  abi,
+  abi as gatewayAbi,
 } from "@lacrypta/bar-gateway/deployments/matic/BarGateway.json";
+
+import ERC20PermitAbi from "../../abi/ERC20Permit.json";
+
 import { ISignature } from "../../plugins/gateway/types/Signature";
-import { BarGateway } from "@lacrypta/bar-gateway/typechain/BarGateway";
+import { IPermit } from "../../types/crypto";
 // import {} from "@lacrypta/bar-gateway/";
 
 // contract details
 const RPC_ADDRESS = process.env.NEXT_PUBLIC_RPC_ADDRESS || "";
+const PERONIO_ADDRESS = process.env.NEXT_PUBLIC_PERONIO_CONTRACT || "";
 const GATEWAY_ADDRESS = process.env.NEXT_PUBLIC_GATEWAY_CONTRACT || address;
 const CALLER_PRIVATE_KEY = process.env.CALLER_PRIVATE_KEY || "";
 
@@ -19,7 +23,9 @@ const provider = new ethers.providers.JsonRpcProvider(RPC_ADDRESS);
 const signer = new ethers.Wallet(CALLER_PRIVATE_KEY, provider);
 
 // Bar Contract
-const contract = new BarGateway(GATEWAY_ADDRESS, abi, signer);
+const gatewayContract = new Contract(GATEWAY_ADDRESS, gatewayAbi, signer);
+// Token Contract
+const tokenContract = new Contract(PERONIO_ADDRESS, ERC20PermitAbi, signer);
 
 const transferAbi = [
   "event Transfer(address indexed from, address indexed to, uint value)",
@@ -51,13 +57,21 @@ export async function serveVoucher(
   voucher: ITransferVoucher,
   signature: ISignature
 ): Promise<string> {
-  const { r, s, v } = signature;
-  // contract["serveVoucher((uint32,uint256,uint256,bytes,bytes),bytes)"](voucher, signature.r);
-  const tx = contract[
-    "serveVoucher((uint32,uint256,uint256,bytes,bytes),bytes32,bytes32,uint8)"
-  ](voucher, r, s, v);
+  const tx = gatewayContract[
+    "serveVoucher((uint32,uint256,uint256,bytes,bytes),bytes)"
+  ](voucher, signature.full);
 
   console.info("Broadcasting tx...");
   console.dir(await tx);
   return (await tx).hash;
+}
+
+export async function runPermit(permit: IPermit) {
+  const { owner, spender, value, deadline, v, r, s } = permit;
+
+  console.info("Executing PERMIT on", tokenContract.address);
+  console.info("PERMIT:");
+  console.dir(permit);
+
+  return tokenContract.permit(owner, spender, value, deadline, v, r, s);
 }

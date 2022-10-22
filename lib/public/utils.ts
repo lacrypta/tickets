107c2cf706de136b7dd29fd0da534690ac492280
Agustin Kassis <webmaster@masize.com>
2022-10-22 12:18:16 -0300

    added generateMessage on utls

--- lib/public/utils.ts
+++ lib/public/utils.ts
@@ -1,5 +1,6 @@
 import { ITransferVoucherSignedStringified } from "./../../plugins/gateway/types/Voucher";
 import { ITransferVoucherSigned } from "../../plugins/gateway/types/Voucher";
+import { ICart } from "../../types/cart";
 
 const generatePermitData = (
   contractAddress?: string,
@@ -19,7 +20,7 @@ const formatVoucher = (
   voucher: ITransferVoucherSigned
 ): ITransferVoucherSignedStringified => {
   const { deadline, metadata, nonce, payload, tag } = voucher.voucher;
-  const { r, s, v } = voucher.signature;
+  const { r, s, v, full } = voucher.signature;
   return {
     voucher: {
       deadline: deadline.toString(),
@@ -32,8 +33,14 @@ const formatVoucher = (
       r,
       s,
       v,
+      full,
     },
   };
 };
 
-export { generatePermitData, formatVoucher };
+const generateMessage = (orderId: string, total: string, _cart?: ICart) => {
+  const formattedAmount = parseFloat(total).toFixed(2);
+  return `Pagar la cuenta de la Orden #${orderId}\nMonto: ${formattedAmount}`;
+};
+
+export { generatePermitData, formatVoucher, generateMessage };

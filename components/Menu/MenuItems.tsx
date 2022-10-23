8f3ddece1e5bbec3900f2002f8609a0a9b7c0947
Agustin Kassis <webmaster@masize.com>
2022-10-23 20:51:46 -0300

    added categories

--- components/Menu/MenuItems.tsx
+++ components/Menu/MenuItems.tsx
@@ -1,22 +1,37 @@
 import styled from "@emotion/styled";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/Cart";
+import { categories } from "../../lib/public/menu";
 import { ICartItem } from "../../types/cart";
 import { MenuItem } from "./MenuItem";
 
 const Container = styled.div``;
 
+const CategoryDiv = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  margin: 10px 0px 10px 0px;
+`;
+
 export const MenuItems = () => {
   const { cart } = useContext(CartContext);
   const cartItems = cart.items ? Object.values(cart.items) : [];
+
   return (
     <Container>
-      {cartItems.map((cartItem: ICartItem) => (
-        <MenuItem
-          key={cartItem.product.id}
-          item={cartItem.product}
-          qty={cartItem.qty}
-        />
+      {categories.map((category, k) => (
+        <div key={k}>
+          <CategoryDiv>{category.name}</CategoryDiv>
+          {cartItems
+            .filter((item) => item.product.cat === category.id)
+            .map((cartItem: ICartItem) => (
+              <MenuItem
+                key={cartItem.product.id}
+                item={cartItem.product}
+                qty={cartItem.qty}
+              />
+            ))}
+        </div>
       ))}
     </Container>
   );

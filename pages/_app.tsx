184118f7fe4a34e4a1b087a56c599c257fd737bb
Agustin Kassis <webmaster@masize.com>
2022-12-26 15:25:13 -0300

    set public provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,8 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+// import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
 
 import { LoadingProvider } from "../contexts/Loading";
 import { Background, Footer, HeaderLogo } from "../components/common";
@@ -26,12 +27,12 @@ const { chains, provider, webSocketProvider } = configureChains(
     //   // You can get your own at https://dashboard.alchemyapi.io
     //   apiKey: "_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC",
     // }),
-    // publicProvider(),
-    jsonRpcProvider({
-      rpc: () => ({
-        http: process.env.NEXT_PUBLIC_RPC_ADDRESS || `http://127.0.0.1:8545/`,
-      }),
-    }),
+    publicProvider(),
+    // jsonRpcProvider({
+    //   rpc: () => ({
+    //     http: process.env.NEXT_PUBLIC_RPC_ADDRESS || `http://127.0.0.1:8545/`,
+    //   }),
+    // }),
   ]
 );
 

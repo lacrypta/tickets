d7d04bcc9e844de62fab4579eaa6e9561c65f135
Agustin Kassis <webmaster@masize.com>
2022-12-26 18:15:41 -0300

    index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Card from "../components/common/Card";
 
 import useLoading from "../hooks/useLoading";
 
 const Home: NextPage = () => {
   const { setActive } = useLoading();
+  const router = useRouter();
+
+  const [show, setShow] = useState(false);
+
+  function nextStep() {
+    setShow(false);
+    setTimeout(() => {
+      router.push("/pago");
+    }, 350);
+  }
 
   useEffect(() => {
     setActive(false);
+    setShow(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -20,14 +33,25 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div
-        className='rounded-md
-            backdrop-blur-sm bg-white/[0.08]
-            h-[30rem] mt-8 p-8 mb-14
-          '
-      >
-        Hola
-      </div>
+      <Card show={show}>
+        <h1>Formulario</h1>
+        <div>
+          Nombre: <input type='text' />
+        </div>
+        <div>
+          E-mail <input type='text' />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              nextStep();
+            }}
+            className='bg-red-500 text-white rounded-md px-4 py-2 w-full'
+          >
+            Comprar
+          </button>
+        </div>
+      </Card>
     </div>
   );
 };

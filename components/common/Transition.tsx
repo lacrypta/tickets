d1c13387b03bd294d177694d0bda079e276e82f7
Agustin Kassis <webmaster@masize.com>
2022-12-27 17:21:47 -0300

    added Transition component

---
+++ components/common/Transition.tsx
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
+import React from "react";
+
+interface ITransitionProps {
+  children: React.ReactNode;
+}
+
+const variants = {
+  in: {
+    transform: "rotateY(90deg)",
+  },
+  out: {
+    transform: "rotateY(-90deg)",
+  },
+  inactive: {
+    transform: "rotateY(0deg)",
+    transition: {
+      duration: 0.35,
+      delay: 0.35,
+    },
+  },
+};
+
+const Wrapper = styled.div`
+  perspective: 1000px;
+  position: relative;
+  width: 100%;
+  top: 0;
+`;
+
+const MotionDiv = styled(motion.div)`
+  position: relative;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  margin: 0 auto;
+  transform-style: preserve-3d;
+  transform: rotateY(0deg);
+`;
+
+export const Transition = ({ children }: ITransitionProps) => {
+  const { asPath } = useRouter();
+  return (
+    <Wrapper>
+      <AnimatePresence>
+        <MotionDiv
+          key={asPath}
+          variants={variants}
+          animate='inactive'
+          initial='in'
+          exit='out'
+          transition={{ duration: 0.35 }}
+          className='backdrop-blur-sm'
+        >
+          {children}
+        </MotionDiv>
+      </AnimatePresence>
+    </Wrapper>
+  );
+};
+
+export default Transition;

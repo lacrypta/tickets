b01081198171dd5f2089368dbcbf88b3910858cd
Agustin Kassis <webmaster@masize.com>
2023-01-04 15:56:35 -0300

    remove shadow class on background

--- components/common/Background.tsx
+++ components/common/Background.tsx
@@ -3,7 +3,7 @@ import SVGSkin from "../../public/images/skin.svg";
 export const Background = () => {
   return (
     <div className='flex justify-center inset-0 overflow-hidden absolute'>
-      <SVGSkin className='opacity-[0.1] scale-[2] h-screen svg-shadow' />
+      <SVGSkin className='opacity-[0.1] scale-[2] h-screen' />
     </div>
   );
 };

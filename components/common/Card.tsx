4e6211f2901b0058f2bc7994d1f92792fd9af73e
Agustin Kassis <webmaster@masize.com>
2023-01-04 15:57:07 -0300

    updated Card for flex style

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -9,7 +9,8 @@ const Card = ({ children }: ICardProps) => {
     <div
       className='bg-white/[0.08] backdrop-blur-sm
           rounded-md ring-2 ring-white/10
-          h-[30rem] p-8 mt-8 mb-14 z-'
+          h-[30rem] p-8 mt-8 mb-14
+          flex flex-col space-y-6'
     >
       {children}
     </div>

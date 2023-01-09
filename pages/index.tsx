bb9cf1be24f16e917ebe192d81702be8cbf3e43a
Agustin Kassis <webmaster@masize.com>
2023-01-09 15:44:05 -0300

    get form data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,33 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Card from "../components/common/Card";
 import Button from "../components/Form/Button";
 import Input from "../components/Form/Input";
 
 import useLoading from "../hooks/useLoading";
 
+interface FormTargetProps extends EventTarget {
+  name: HTMLInputElement;
+  email: HTMLInputElement;
+}
+
 const Home: NextPage = () => {
   const { setActive } = useLoading();
   const router = useRouter();
 
-  function nextStep() {
+  function nextStep(e: React.FormEvent<HTMLFormElement>) {
     router.push("/pago", undefined, { scroll: false });
+
+    const target = e.target as FormTargetProps;
+
+    const formData = {
+      name: target.name.value,
+      email: target.email.value,
+    };
+    console.dir(formData);
+    e?.preventDefault();
   }
 
   useEffect(() => {
@@ -29,24 +43,27 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Card>
-        <h1>Completá tus datos</h1>
-        <Input
-          label='Nombre Completo'
-          name='name'
-          placeholder='Nombre Completo'
-        />
-        <Input label='E-mail' name='email' type='email' placeholder='E-mail' />
+      <form onSubmit={nextStep}>
+        <Card>
+          <h1>Completá tus datos</h1>
 
-        <div className='space-y-2'></div>
-        <Button
-          onClick={() => {
-            nextStep();
-          }}
-        >
-          Comprar
-        </Button>
-      </Card>
+          <Input
+            label='Nombre Completo'
+            name='name'
+            placeholder='Nombre Completo'
+          />
+
+          <Input
+            label='E-mail'
+            name='email'
+            type='email'
+            placeholder='E-mail'
+          />
+
+          <div className='space-y-2'></div>
+          <Button type='submit'>Comprar</Button>
+        </Card>
+      </form>
     </div>
   );
 };

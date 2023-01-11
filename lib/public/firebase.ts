3a60ecc04a0ace89dd2c0fffbc9e6b23950b509d
Agustin Kassis <webmaster@masize.com>
2023-01-11 17:19:22 -0300

    updated firebase config

--- lib/public/firebase.ts
+++ lib/public/firebase.ts
@@ -1,23 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "@firebase/app";
-import { getFirestore, collection, onSnapshot, doc } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  doc,
+  addDoc,
+} from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0rA8BNwjTrnl9w6jg3SHiOYzNOrpAdRc",
-  authDomain: "la-crypta---eventos.firebaseapp.com",
-  projectId: "la-crypta---eventos",
-  storageBucket: "la-crypta---eventos.appspot.com",
-  messagingSenderId: "554731055448",
-  appId: "1:554731055448:web:7d790caab5bb772a841d2d",
-  measurementId: "G-CRM9KKMZ3C",
+  apiKey: "AIzaSyC62592R46yS8ubM6eCiY0QZN6n7hBjGSk",
+  authDomain: "la-crypta---eventos-c5209.firebaseapp.com",
+  projectId: "la-crypta---eventos-c5209",
+  storageBucket: "la-crypta---eventos-c5209.appspot.com",
+  messagingSenderId: "212815203220",
+  appId: "1:212815203220:web:edec87d0149bda478add81",
+  measurementId: "G-FFJM60THJD",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, onSnapshot, doc };
+export { db, collection, onSnapshot, doc, addDoc };

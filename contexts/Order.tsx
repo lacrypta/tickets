a5ef1075603fc9a3f1af9727cda93669d5de20d5
Agustin Kassis <webmaster@masize.com>
2023-01-11 17:18:57 -0300

    added order context logic

--- contexts/Order.tsx
+++ contexts/Order.tsx
@@ -1,11 +1,56 @@
-import { createContext } from "react";
+import React, {
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react";
+import { IOrder } from "../types/order";
+import { doc, onSnapshot, db } from "../lib/public/firebase";
 
-interface OrderContextType {}
+interface OrderContextType {
+  order?: IOrder;
+  setOrder: Dispatch<SetStateAction<IOrder | undefined>>;
+}
 
 export const OrderContext = createContext<OrderContextType>({});
 
 export const OrderProvider = ({ children }: any) => {
-  return <OrderContext.Provider value={{}}>{children}</OrderContext.Provider>;
+  const [order, setOrder] = useState<IOrder>();
+  const [listeningOrderId, setListeningOrderId] = useState<string>();
+
+  // Subscribe order snapshots
+  const subscribeChanges = async () => {
+    const orderId = order?.id as string;
+    console.info(`Listening for changes (${orderId})...`);
+
+    if (listeningOrderId === orderId) return;
+    setListeningOrderId(orderId);
+
+    return onSnapshot(doc(db, "orders", orderId), (doc) => {
+      const data = doc.data() as IOrder;
+      data.id = doc.id;
+      setOrder(data);
+    });
+  };
+
+  useEffect(() => {
+    // Prevent subscribe if its already listening to the orderId
+    if (!order?.id || listeningOrderId === order?.id) {
+      return;
+    }
+
+    subscribeChanges();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [order, listeningOrderId]);
+
+  useEffect;
+
+  return (
+    <OrderContext.Provider value={{ order, setOrder }}>
+      {children}
+    </OrderContext.Provider>
+  );
 };
 
 export default OrderContext;

bb7f98ab2b2c521abb3ecfac2c0032c6e74a1ff8
Agustin Kassis <webmaster@masize.com>
2023-01-12 19:09:16 -0300

    createGenericOrder

--- hooks/useOrder.ts
+++ hooks/useOrder.ts
@@ -1,13 +1,15 @@
+import { IPayment } from "./../types/payment";
 import { OrderContext } from "./../contexts/Order";
 import { useContext } from "react";
 import { IOrder } from "../types/order";
 
-import { addDoc, collection, db } from "../lib/public/firebase";
+import { addDoc, updateDoc, collection, db, doc } from "../lib/public/firebase";
 import { IUser } from "../types/user";
 
 export interface IUseOrderResult {
   order?: IOrder;
   createOrder: (_order: IUser) => Promise<IOrder>;
+  createPayment: (_payment: IPayment) => Promise<IPayment>;
 }
 
 const useOrder = (): IUseOrderResult => {
@@ -19,18 +21,37 @@ const useOrder = (): IUseOrderResult => {
       status: "pending",
     };
 
-    console.info("Order data:");
-    console.dir(order);
-
     const docRef = await addDoc(collection(db, "orders"), order);
     order.id = docRef.id;
     setOrder(order);
     return order;
   };
 
+  const createPayment = async (payment: IPayment): Promise<IPayment> => {
+    // Create payment in firestore
+
+    const data = {
+      ...payment,
+      status: "waiting", // Makes sure its in waiting status
+      orderId: order?.id,
+    };
+
+    console.dir(data);
+
+    const paymentRef = addDoc(collection(db, "payments"), data);
+    updateDoc(doc(db, "orders/" + order?.id), {
+      status: "processing",
+    });
+
+    console.dir(paymentRef);
+
+    return payment;
+  };
+
   return {
     order,
     createOrder,
+    createPayment,
   };
 };
 

f396c839bd9cff7e1d2982a99b68b95502c79cf4
Agustin Kassis <webmaster@masize.com>
2023-01-13 01:05:21 -0300

    subscribe payment

--- contexts/Order.tsx
+++ contexts/Order.tsx
@@ -4,12 +4,15 @@ import React, {
   Dispatch,
   SetStateAction,
   useEffect,
+  useCallback,
 } from "react";
 import { IOrder } from "../types/order";
 import { doc, onSnapshot, db } from "../lib/public/firebase";
+import { IPayment } from "../types/payment";
 
 interface OrderContextType {
   order?: IOrder;
+  payment?: IPayment;
   setOrder: Dispatch<SetStateAction<IOrder | undefined>>;
 }
 
@@ -19,21 +22,53 @@ export const OrderContext = createContext<OrderContextType>({
 
 export const OrderProvider = ({ children }: any) => {
   const [order, setOrder] = useState<IOrder>();
+  const [payment, setPayment] = useState<IPayment>();
+
   const [listeningOrderId, setListeningOrderId] = useState<string>();
+  const [paymentListener, setPaymentListener] = useState<any>();
+
+  const onOrderChange = (doc: any) => {
+    const data = doc.data() as IOrder;
+    data.id = doc.id;
+
+    // If paymentId is found, subscribe payment listener
+    if (data.paymentId && data.paymentId !== payment?.id) {
+      console.dir("New PaymentId found!");
+      paymentListener && paymentListener(); // Unsubscribe previous payment listener
+      setPaymentListener(subscribePayment(data.paymentId));
+    }
+
+    // If no payment is found, unsubscribe payment listener
+    if (!data.paymentId) {
+      paymentListener && paymentListener(); // Unsubscribe previous payment listener
+    }
+
+    setOrder(data);
+  };
+
+  const onPaymentChange = (doc: any) => {
+    const data = doc.data() as IPayment;
+    data.id = doc.id;
+    setPayment(data);
+    console.dir(data);
+  };
 
   // Subscribe order snapshots
-  const subscribeChanges = async () => {
+  const subscribeOrder = useCallback(async () => {
     const orderId = order?.id as string;
     console.info(`Listening for changes (${orderId})...`);
 
     if (listeningOrderId === orderId) return;
     setListeningOrderId(orderId);
 
-    return onSnapshot(doc(db, "orders", orderId), (doc) => {
-      const data = doc.data() as IOrder;
-      data.id = doc.id;
-      setOrder(data);
-    });
+    return onSnapshot(doc(db, "orders", orderId), onOrderChange);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [order, listeningOrderId, paymentListener]);
+
+  // Subscribe payment snapshots
+  const subscribePayment = async (paymentId: string) => {
+    console.info("Subscribe for PaymentID", paymentId);
+    return onSnapshot(doc(db, "payments", paymentId), onPaymentChange);
   };
 
   useEffect(() => {
@@ -41,15 +76,14 @@ export const OrderProvider = ({ children }: any) => {
     if (!order?.id || listeningOrderId === order?.id) {
       return;
     }
-
-    subscribeChanges();
+    subscribeOrder();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [order, listeningOrderId]);
 
   useEffect;
 
   return (
-    <OrderContext.Provider value={{ order, setOrder }}>
+    <OrderContext.Provider value={{ order, payment, setOrder }}>
       {children}
     </OrderContext.Provider>
   );

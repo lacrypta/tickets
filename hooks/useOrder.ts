356e8e2e15c450115e6146cf22de93ec9c2ae2da
Agustin Kassis <webmaster@masize.com>
2023-01-20 20:38:57 -0300

    create order on hook

--- hooks/useOrder.ts
+++ hooks/useOrder.ts
@@ -1,61 +1,54 @@
 import { IPayment } from "./../types/payment";
 import { OrderContext } from "./../contexts/Order";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { IOrder } from "../types/order";
-
-import {
-  addDoc,
-  updateDoc,
-  collection,
-  db,
-  doc,
-  deleteDoc,
-} from "../lib/public/firebase";
 import { IUser } from "../types/user";
 
 export interface IUseOrderResult {
   order?: IOrder;
+  payments?: IPayment[];
+  activePayments?: IPayment[];
   createOrder: (_order: IUser) => Promise<IOrder>;
   createPayment: (_payment: IPayment) => Promise<IPayment>;
 }
 
 const useOrder = (): IUseOrderResult => {
-  const { order, payment: currentPayment, setOrder } = useContext(OrderContext);
+  const { order, payments, addOrder, addPayment } = useContext(OrderContext);
 
-  const createOrder = async (userData: IUser): Promise<IOrder> => {
-    const order: IOrder = {
-      user: userData,
-      status: "pending",
-    };
+  const activePayments = payments?.filter((payment) => {
+    return payment.status === "waiting";
+  });
 
-    const docRef = await addDoc(collection(db, "orders"), order);
-    order.id = docRef.id;
-    setOrder(order);
-    return order;
-  };
+  // Creates order if it doesn't exist
+  const createOrder = useCallback(
+    async (user: IUser) => {
+      if (order && ["pending", "processing"].includes(order.status)) {
+        console.warn("Order already exists");
+        return order;
+      }
+      return addOrder(user);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [order]
+  );
 
-  const createPayment = async (payment: IPayment): Promise<IPayment> => {
-    // Create payment in firestore
-    const data = {
-      ...payment,
-      status: "waiting", // Makes sure its in waiting status
-      orderId: order?.id,
-    };
-
-    if (currentPayment) {
-      await deleteDoc(doc(db, "payments" + currentPayment.id));
-    }
-    const paymentRef = await addDoc(collection(db, "payments"), data);
-    updateDoc(doc(db, "orders/" + order?.id), {
-      status: "processing",
-      paymentId: paymentRef.id,
-    });
-
-    return payment;
-  };
+  // Creates payment if it doesn't exist
+  const createPayment = useCallback(
+    async (payment: IPayment): Promise<IPayment> => {
+      // Check if payment with the method already exists
+      const oldPayment = activePayments?.find(
+        (el) => el.method === payment.method
+      );
+      return oldPayment || addPayment(payment);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [activePayments]
+  );
 
   return {
     order,
+    payments,
+    activePayments,
     createOrder,
     createPayment,
   };

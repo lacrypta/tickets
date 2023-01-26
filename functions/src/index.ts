2388f6c68364311fd5f21d4ebae1bcad7e8efeb0
Agustin Kassis <webmaster@masize.com>
2023-01-26 10:42:19 -0300

    create functions url env

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+process.env.FUNCTIONS_URL = `https://${process.env.CLOUD_FUNCTIONS_REGION}-${process.env.GCLOUD_PROJECT}.cloudfunctions.net/`;
+
 // Gateways
 export * from "./gateways/mercadopago";

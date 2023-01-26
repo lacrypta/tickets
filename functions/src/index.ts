04afadc6f835613e55b1edd931d7d71285dfd7fa
Agustin Kassis <webmaster@masize.com>
2023-01-26 18:21:11 -0300

    removed functions env from index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,5 @@
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-process.env.FUNCTIONS_URL = `https://${process.env.CLOUD_FUNCTIONS_REGION}-${process.env.GCLOUD_PROJECT}.cloudfunctions.net/`;
-
 // Gateways
 export * from "./gateways/mercadopago";

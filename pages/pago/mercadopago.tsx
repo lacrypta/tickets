3b0919e4fc212ab6047d8ae4dc57bcf7c9c1809e
Agustin Kassis <webmaster@masize.com>
2023-01-26 10:44:30 -0300

    added mercadopago button

--- pages/pago/mercadopago.tsx
+++ pages/pago/mercadopago.tsx
@@ -8,16 +8,44 @@ import Button from "../../components/Form/Button";
 import useOrder from "../../hooks/useOrder";
 import { useRedirectOnEmpty } from "../../hooks/useRedirectOnEmpty";
 
+import { useMercadopago } from "react-sdk-mercadopago";
+import { useEffect, useState } from "react";
+
 const PRICE = parseFloat(process.env.NEXT_PUBLIC_TICKET_PRICE || "2000");
 
 const Home: NextPage = () => {
   const { payment } = useOrder();
   const router = useRouter();
+  const mercadopago = useMercadopago.v2(
+    process.env.NEXT_PUBLIC_MP_PUBLIC_KEY as string,
+    {
+      locale: "es-AR",
+    }
+  );
+  const [checkoutObject, setCheckoutObject] = useState<any>();
+  const [mounted, setMounted] = useState(false);
 
   useRedirectOnEmpty(["order", "payment"]);
 
+  // MercadoPago Checkout Pro
+  useEffect(() => {
+    if (payment?.preference_id && mercadopago && !mounted) {
+      setMounted(true);
+
+      const checkout = mercadopago.checkout({
+        preference: {
+          id: payment?.preference_id,
+        },
+      });
+
+      setCheckoutObject(checkout);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [payment?.preference_id, mercadopago, mounted]);
+
   function nextStep() {
-    router.push("/pagado");
+    checkoutObject.open();
+    // router.push("/pagado");
   }
 
   return (

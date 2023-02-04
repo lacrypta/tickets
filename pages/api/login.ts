ad69ff8edef3ad50ff02ed261ae3200364cac620
Agustin Kassis <webmaster@masize.com>
2023-02-04 19:55:04 -0300

    added login api

---
+++ pages/api/login.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getAuth } from "../../lib/private/firebase";
+import { ResponseType } from "../../types/request";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  try {
+    if (req.method !== "POST") {
+      throw new Error("Invalid Method`");
+    }
+
+    if (req.body.password !== process.env.PASSWORD) {
+      throw new Error("Invalid Password");
+    }
+
+    const userId = "admin";
+    const additionalClaims = {
+      adminAccount: true,
+    };
+
+    const token = await getAuth().createCustomToken(userId, additionalClaims);
+
+    res.status(200).json({ success: true, data: { token } });
+  } catch (e: any) {
+    console.error(e);
+    res.status(400).json({ success: false });
+  }
+}

b03ba810edd0b4be576b65507122aa47f2a42aa2
Agustin Kassis <webmaster@masize.com>
2023-02-04 19:55:21 -0300

    aded type on input

--- components/Form/Input.tsx
+++ components/Form/Input.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  type?: string;
   name: string;
 }
 
-export const Input = ({ label, ...props }: InputProps) => {
+export const Input = ({ label, type = "text", ...props }: InputProps) => {
   return (
     <div>
       <label className='block text-sm font-medium text-gray-100'>{label}</label>
       <div className='relative mt-1 rounded-md shadow-sm'>
         <input
-          type='text'
+          type={type}
           {...props}
           className='block w-full rounded-md text-black border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
         />

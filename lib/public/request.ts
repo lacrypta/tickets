b539f0368b3c2d8eae6335d34c67b935f5229a73
Agustin Kassis <webmaster@masize.com>
2023-02-04 19:54:47 -0300

    changed API for next

--- lib/public/request.ts
+++ lib/public/request.ts
@@ -1,9 +1,7 @@
 import { ResponseType } from "../../types/request";
-import { firebaseConfig } from "./firebase";
-const API_URL = `https://${CLOUD_FUNCTIONS_REGION}-${firebaseConfig.projectId}.cloudfunctions.net/`;
 
 const ajaxCall = async (path: string, data: any): Promise<ResponseType> => {
-  const res = await fetch(API_URL + path, {
+  const res = await fetch("/api/" + path, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",

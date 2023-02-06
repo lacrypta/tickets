305c71024e245781cf3532a48ff17ff9fea200b8
Agustin Kassis <webmaster@masize.com>
2023-02-05 23:46:31 -0300

    starting Qr Scanner

--- components/Admin/MainWidget.tsx
+++ components/Admin/MainWidget.tsx
@@ -1,5 +1,21 @@
+import { getAuth } from "../../lib/public/firebase";
+import Button from "../Form/Button";
+import TicketScanner from "./TicketScanner";
+
 export const MainWidget = () => {
-  return <div className='h-16'>Logueado como admin</div>;
+  const auth = getAuth();
+
+  const handleLogout = () => {
+    console.info("Cerrado!");
+    auth.signOut();
+  };
+
+  return (
+    <div>
+      <Button onClick={handleLogout}>Cerrar</Button>
+      <TicketScanner />
+    </div>
+  );
 };
 
 export default MainWidget;

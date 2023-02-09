a2c2b7966d57d846639f6b9c5347a797693cfd82
Agustin Kassis <webmaster@masize.com>
2023-02-09 10:55:00 -0300

    removed hash callback

--- components/Admin/MainWidget.tsx
+++ components/Admin/MainWidget.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { QrScannerContext } from "../../contexts/QrScanner";
 import { getAuth } from "../../lib/public/firebase";
 import Button from "../Form/Button";
@@ -6,15 +6,6 @@ import Button from "../Form/Button";
 export const MainWidget = () => {
   const auth = getAuth();
   const qrScanner = useContext(QrScannerContext);
-
-  const onHash = (hash: string) => {
-    console.info("Hash: ", hash);
-  };
-
-  useEffect(() => {
-    qrScanner.setCallback(onHash);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const handleLogout = () => {
     console.info("Cerrado!");

9ce4aaf486eec5f03c8a90032dfc33a6dc27bddf
Agustin Kassis <webmaster@masize.com>
2023-02-09 18:46:24 -0300

    added fixture api

---
+++ pages/api/fixture.ts
@@ -0,0 +1,40 @@
+import { db } from "../../lib/private/firebase";
+import fixture from "../../data/fixture";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ResponseType } from "../../types/request";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  try {
+    if (!process.env.NEXT_PUBLIC_DEBUG) {
+      throw new Error("Not in debug mode`");
+    }
+
+    const { orders, payments, purchases } = fixture;
+
+    // Add orders to database
+    orders.forEach((order) => {
+      const orderRef = db.collection("orders").doc(order._id);
+      orderRef.set(order);
+    });
+
+    // Add payments to database
+    payments.forEach((payment) => {
+      const paymentRef = db.collection("payments").doc(payment._id);
+      paymentRef.set(payment);
+    });
+
+    // Add purchases to database
+    purchases.forEach((purchase) => {
+      const purchaseRef = db.collection("purchases").doc(purchase._id);
+      purchaseRef.set(purchase);
+    });
+
+    res.status(200).json({ success: true, data: { fixture } });
+  } catch (e: any) {
+    console.error(e);
+    res.status(400).json({ success: false, message: e.message });
+  }
+}

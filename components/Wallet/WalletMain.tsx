4561c93414e30f13700009f472b401c261d8033f
Agustin Kassis <webmaster@masize.com>
2023-02-10 20:11:35 -0300

    renamed wallet main

---
+++ components/Wallet/WalletMain.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import { useCallback, useContext } from "react";
+import { PurchaseContext } from "../../contexts/Purchase";
+import Button from "../Form/Button";
+import MuunSvg from "../../public/images/muun.svg";
+
+export const WalletMain = () => {
+  const { purchase } = useContext(PurchaseContext);
+  const router = useRouter();
+  const nextStep = useCallback(() => {
+    router.push("wallet/muun");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <h2 className='text-2xl'>Bienvenido {purchase?.user.fullname}!</h2>
+      <div className='flex flex-col space-y-4 pt-4'>
+        <p>Vas a recibir el equivalente de $1000 en Bitcoin</p>
+        <p>
+          Descargá <b>Muun</b> y seguí los pasos de la App
+        </p>
+        <p>Volvé a esta página</p>
+
+        <Button onClick={nextStep}>
+          {" "}
+          Descargar <MuunSvg height='48' className='mr-2' />
+        </Button>
+
+        <Button onClick={nextStep}>Ya está configurada</Button>
+      </div>
+    </div>
+  );
+};
+
+export default WalletMain;

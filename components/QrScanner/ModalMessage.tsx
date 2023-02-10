052c1d0a09fc547c26fc5c20503ce34f8043b2ca
Agustin Kassis <webmaster@masize.com>
2023-02-10 17:31:07 -0300

    updated modal css style

--- components/QrScanner/ModalMessage.tsx
+++ components/QrScanner/ModalMessage.tsx
@@ -7,17 +7,18 @@ interface IModalMessageProps {
 
 export const ModalMessage = ({ children }: IModalMessageProps) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0 }}
-      transition={{ duration: 0.3 }}
-      className='absolute inset-x-0 bottom-0 h-[30%] z-20 
-flex flex-col justify-center text-center
-bg-white/[0.25] backdrop-blur-md'
-    >
-      {children}
-    </motion.div>
+    <div className='absolute top-[20%] left-[50%] translate-x-[-50%] z-20 w-96'>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0, height: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        className='flex flex-col justify-center overflow-hidden text-center text-black
+ bg-white/[0.70] p-2 pb-5 rounded-md drop-shadow-md'
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 };
 

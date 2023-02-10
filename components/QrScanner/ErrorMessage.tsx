d281a147630a924a1951e70998c166e765d6594b
Agustin Kassis <webmaster@masize.com>
2023-02-10 17:31:19 -0300

    added error animation on Error message for qr

--- components/QrScanner/ErrorMessage.tsx
+++ components/QrScanner/ErrorMessage.tsx
@@ -1,10 +1,25 @@
 import ModalMessage from "./ModalMessage";
+import { motion } from "framer-motion";
+import ErrorIcon from "./ErrorIcon";
 
 export const ErrorMessage = ({ message }: { message: string }) => {
   return (
     <ModalMessage>
-      <h1 className='block'>Error</h1>
-      <div>{message}</div>
+      <div className='text-3xl bg-red-600 text-white rounded-sm mb-4 p-2'>
+        Error
+      </div>
+      <div className='flex flex-col space-y-4 text-left pl-3'>
+        <div>
+          <b>{message}</b>
+        </div>
+      </div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ bounce: 0.25, duration: 0.5, delay: 0.3 }}
+      >
+        <ErrorIcon />
+      </motion.div>
     </ModalMessage>
   );
 };

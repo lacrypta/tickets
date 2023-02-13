4337ec96c606f077585223b4d529dad543663c3b
Agustin Kassis <webmaster@masize.com>
2023-02-13 11:11:06 -0300

    added lnbits lib

---
+++ functions/src/lib/lnbits.ts
@@ -0,0 +1,29 @@
+import { LnUrlWithdrawBody, LnUrlWithdrawData } from "./../../../types/lnbits";
+import axios from "axios";
+
+export const generateWithdrawLink = async (sats: number) => {
+  const url = process.env.LNBITS_URL + "withdraw/api/v1/links";
+  const body: LnUrlWithdrawBody = {
+    title: "Curso de La Crypta",
+    max_withdrawable: sats,
+    min_withdrawable: sats,
+    is_unique: true,
+    uses: 1,
+    wait_time: 1,
+    // webhook_url:
+  };
+
+  const { data, status } = await axios.post<LnUrlWithdrawData>(url, body, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Api-Key": process.env.LNBITS_API_KEY || "",
+    },
+  });
+
+  if (status !== 201) {
+    throw new Error("Invalid");
+  }
+
+  return data.lnurl;
+};

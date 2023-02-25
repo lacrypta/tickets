b61aa9b4b9abc11d3e1f5d0630b6d81e35747374
Agustin Kassis <webmaster@masize.com>
2023-02-25 04:24:02 -0300

    updated link processing ui

--- components/Admin/MercadoPagoLinks/ProcessingLink.tsx
+++ components/Admin/MercadoPagoLinks/ProcessingLink.tsx
@@ -1,3 +1,8 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useCallback, useEffect, useState } from "react";
+import { ajaxCall } from "../../../lib/public/request";
+import ElementAnimation from "./ElementAnimation";
+
 export type LinkProcessStatus =
   | "pending"
   | "processing"
@@ -6,23 +11,61 @@ export type LinkProcessStatus =
   | "cancelled"
   | "invalid";
 
-export interface LinkProcess {
-  url: string;
-  preference_id?: string;
-  status: LinkProcessStatus;
-}
-
 interface ProcessingLinkProps {
-  link: LinkProcess;
+  link: string;
+  index: number;
+  active: boolean;
+  onFinished: () => void;
 }
 
-export const ProcessingLink = ({ link }: ProcessingLinkProps) => {
+export const ProcessingLink = ({
+  link,
+  index,
+  active,
+  onFinished,
+}: ProcessingLinkProps) => {
+  const [status, setStatus] = useState<LinkProcessStatus>("pending");
+
+  useEffect(() => {
+    if (active) {
+      processLink(link);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [link, active]);
+
+  const processLink = useCallback(async (link: string) => {
+    setStatus("processing");
+    const res = await ajaxCall("mercadopago", {
+      link,
+    });
+    setTimeout(() => {
+      setStatus("ready");
+      onFinished && onFinished();
+    }, 500);
+
+    console.dir(res);
+  }, []);
+
   return (
-    <div className='flex w-full'>
-      <span>url : {link.url}</span>
-      <span>{link.preference_id}</span>
-      <span>{link.status}</span>
-    </div>
+    <motion.div
+      className='flex items-center pl-4 overflow-hidden relative'
+      initial={{ height: 0, opacity: 0 }}
+      animate={{ height: 40, opacity: 1 }}
+      exit={{ height: 0, opacity: 0 }}
+      transition={{
+        duration: 0.25,
+        delay: 0.15 + 0.15 * index,
+      }}
+    >
+      <span>{link}</span>
+      <span className='w-[40px] h-[40px] relative'>
+        <AnimatePresence>
+          {status === "processing" && <ElementAnimation type='spinner' />}
+          {status === "ready" && <ElementAnimation type='check' />}
+        </AnimatePresence>
+      </span>
+      {/* <span>{link.status}</span> */}
+    </motion.div>
   );
 };
 

bfdc361e79f10bdf42173b0781aa36f29cd4ad7b
Agustin Kassis <webmaster@masize.com>
2023-03-14 17:46:32 -0300

    added whatsapp button

--- components/common/WhatsAppButton.tsx
+++ components/common/WhatsAppButton.tsx
@@ -1,7 +1,21 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
+import whatsAppImage from "../../public/images/whatsapp.png";
 
 export const WhatsAppButton = () => {
-  return <div className='flex justify-center hidden'>Button de Whatsapp</div>;
+  const openWhatsapp = useCallback(() => {
+    const phone = "541131080456";
+    const message = encodeURIComponent(`Hola, necesito ayuda con mi compra.`);
+
+    window.open(
+      `https://api.whatsapp.com/send/?phone=%2B${phone}&text=${message}&type=phone_number&app_absent=0`
+    );
+  }, []);
+
+  return (
+    <button className='flex bg-[#4ec95d] rounded-full w-20 h-20 p-4 object-center items-center justify-center fixed z-50 bottom-4 right-4 hover:scale-110 transition ease-in-out'>
+      <img onClick={openWhatsapp} src={whatsAppImage.src} alt='Ayuda' />
+    </button>
+  );
 };
 
 export default memo(WhatsAppButton);

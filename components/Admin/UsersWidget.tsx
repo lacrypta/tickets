4692729977d59190408f22e926813312eada7a37
Agustin Kassis <webmaster@masize.com>
2023-03-14 16:14:37 -0300

    users widget

--- components/Admin/UsersWidget.tsx
+++ components/Admin/UsersWidget.tsx
@@ -3,9 +3,15 @@ import { IOrder } from "../../types/order";
 import { db, collection, onSnapshot } from "../../lib/public/firebase";
 import { Tab } from "@headlessui/react";
 import TabHeader from "./Users/TabHeader";
+import Order from "./Users/Order";
 
 export const UsersWidget = () => {
-  const [orders, setOrders] = useState<IOrder[]>([]);
+  const [categories, setCategories] = useState<any>({
+    Pendientes: [],
+    Confirmados: [],
+    Cancelados: [],
+  });
+
   useEffect(() => {
     // get orders from firebase
     onSnapshot(collection(db, "orders"), (snapshot) => {
@@ -14,32 +20,33 @@ export const UsersWidget = () => {
       console.dir(snapshot);
       snapshot.forEach((doc) => {
         orders.push({ id: doc.id, ...doc.data() } as IOrder);
-        console.dir(doc);
+        console.info("doc.data()");
+        console.dir(doc.data());
       });
 
-      setOrders(orders);
+      setCategories({
+        Pendientes: orders.filter((order) =>
+          ["pending", "processing"].includes(order.status)
+        ),
+        Confirmados: orders.filter((order) => order.status === "completed"),
+        Cancelados: orders.filter((order) => order.status === "cancelled"),
+      });
     });
   }, []);
 
-  const [categories] = useState({
-    Pendientes: orders,
-    Confirmados: orders,
-    Cancelados: orders,
-  });
   return (
     <div>
-      <div>{JSON.stringify(categories)}</div>
       <Tab.Group>
         <Tab.List className='flex flex-auto w-full space-x-1 rounded-xl bg-blue-900/20 p-1'>
           {Object.keys(categories).map((category, k) => (
             <TabHeader key={k} title={category} />
           ))}
         </Tab.List>
-        <Tab.Panels>
-          {Object.values(categories).map((orders, k) => (
-            <Tab.Panel key={k}>
-              {orders.map((order, k) => (
-                <div key={k}>{JSON.stringify(order)}</div>
+        <Tab.Panels className='mt-5'>
+          {Object.values(categories).map((orders: any, k) => (
+            <Tab.Panel key={k} className='space-y-1'>
+              {orders.map((order: any, k: string) => (
+                <Order key={k} order={order} />
               ))}
             </Tab.Panel>
           ))}
